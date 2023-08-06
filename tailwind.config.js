8f98eaaec82ab39e39ca5d78bcfb2bcecdb62a3e
SoulRvr <pawelc29@interia.pl>
2023-08-06 12:43:00 +0200

    basic

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        Gradient1: "hsl(236, 72%, 79%)",
+        Gradient2: "hsl(237, 63%, 64%)",
+        VeryLightGrayishBlue: "hsl(240, 78%, 98%)",
+        LightGrayishBlue: "hsl(234, 14%, 74%)",
+        GrayishBlue: "hsl(233, 13%, 49%)",
+        DarkGrayishBlue: "hsl(232, 13%, 33%)",
+      },
+      fontFamily: {
+        Montserrat: ["Montserrat", "sans-serif"],
+      },
+      fontSize: {
+        normal: "15px",
+      },
+    },
+  },
+  plugins: [],
+};
